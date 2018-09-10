9f82bf2212308cf3cfcf69137f2dfffceb3f7031
Homem Pedra <cadu.copg.ph@gmail.com>
2018-09-10 22:37:39 +0000

    segundo pedf

---
+++ wp-content/themes/novotema/assets/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <body>
+        
+        <?php get_header();?>
+        
+        <h2>Xesque do bresque</h2>
+        
+        <?php get_footer();?>
+        
+    </body>
+</html>
